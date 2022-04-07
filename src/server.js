0302a325bdbedee93fa614ea95fb773e42defeab
Leafmire <ekwkqdk007@naver.com>
2022-04-07 17:54:40 +0900

    introspection true

--- src/server.js
+++ src/server.js
@@ -20,6 +20,8 @@ async function startServer() {
 
   const server = new ApolloServer({
     schema,
+    playground: true,
+    introspection: true,
     context: async (ctx) => {
       if (ctx.req) {
         return {
